624f07c6f3ecce0252ca996a3a5075cc5da18089
elguiriashing <alexashing1@gmail.com>
2026-08-05 13:32:43 +0200

    Purge example/demo data and link to akipasa.com public web app

---
+++ config.js
@@ -0,0 +1,6 @@
+// Optional cloud configuration and site URLs.
+window.AKIHQ_CONFIG = Object.assign({
+  SITE_URL: "https://akipasa.com",
+  CRM_URL: "https://crm.akipasa.com"
+}, window.AKIHQ_CONFIG || {});
+
